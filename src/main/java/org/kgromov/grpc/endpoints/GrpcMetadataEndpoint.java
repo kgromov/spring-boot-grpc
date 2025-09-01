4e8eb8bb6b9311ad812802b37e4b7ff9fc4ab7c2
konstantin <konst.gromov@gmail.com>
2025-09-02 01:59:59 +0300

    On a way to get fields metadata

--- src/main/java/org/kgromov/grpc/endpoints/GrpcMetadataEndpoint.java
+++ src/main/java/org/kgromov/grpc/endpoints/GrpcMetadataEndpoint.java
@@ -1,16 +1,21 @@
 package org.kgromov.grpc.endpoints;
 
+import com.google.protobuf.Descriptors;
 import io.grpc.MethodDescriptor;
 import io.grpc.ServerServiceDefinition;
 import io.grpc.ServiceDescriptor;
+import io.grpc.protobuf.ProtoUtils;
+import io.grpc.protobuf.lite.ProtoLiteUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.kgromov.grpc.server.GrpcServer;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.core.ResolvableType;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
 import java.util.List;
 
@@ -38,11 +43,27 @@ public class GrpcMetadataEndpoint {
                 .filter(serviceDescriptor -> serviceDescriptor.getName().equals(serviceName))
                 .map(ServiceDescriptor::getMethods)
                 .flatMap(Collection::stream)
-                .peek(md -> log.info("Method schema: {}", md.getSchemaDescriptor()))
-                .map(md -> MethodSchema.builder()
-                        .name(md.getBareMethodName())
-                        .type(md.getType().name())
-                        .build())
+                .peek(md -> {
+//                    try {
+//                        Class<?> messageClass = Class.forName(requestType.getName());
+//                        com.google.protobuf.GeneratedMessage defaultInstance = (com.google.protobuf.GeneratedMessage) messageClass.getMethod("getDefaultInstance").invoke(null);
+//                        Descriptors.FieldDescriptor fieldByNumber = defaultInstance.getDescriptorForType().findFieldByNumber(0)
+//                    } catch (Exception e) {
+//                        throw new RuntimeException(e);
+//                    }
+                })
+                .map(md -> {
+                    MethodDescriptor.Marshaller<?> requestMarshaller = md.getRequestMarshaller();
+                    MethodDescriptor.Marshaller<?> responseMarshaller = md.getResponseMarshaller();
+                    Class<?> requestType = ((MethodDescriptor.ReflectableMarshaller) requestMarshaller).getMessageClass();
+                    Class<?> responseType = ((MethodDescriptor.ReflectableMarshaller) responseMarshaller).getMessageClass();
+                    return MethodSchema.builder()
+                            .name(md.getBareMethodName())
+                            .type(md.getType().name())
+                            .request(MessageSchema.builder().type(requestType.getTypeName()).build())
+                            .response(MessageSchema.builder().type(responseType.getTypeName()).build())
+                            .build();
+                })
                 .toList();
         return ServiceMethods.builder()
                 .name(serviceName)
